6d55444898b36dc5ff18383f70ac5d025475e82e
GHopper <np25071984@gmail.com>
2019-05-29 16:49:29 +0300

    Brand entity

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'MainController@index')->name('main');
+
+    Route::resources([
+        'brand' => 'BrandController',
+    ]);
 });
+
