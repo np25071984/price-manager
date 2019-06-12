678d42d980008653c7c6df68b311962d228920de
GHopper <np25071984@gmail.com>
2019-06-12 19:44:57 +0300

    add column to generated price list

--- app/Jobs/GeneratePrice.php
+++ app/Jobs/GeneratePrice.php
@@ -45,19 +45,24 @@ class GeneratePrice implements ShouldQueue
         $sheet->getColumnDimension('A')->setWidth(10);
         $sheet->setCellValue('B1', 'Бренд');
         $sheet->getColumnDimension('B')->setWidth(25);
-        $sheet->setCellValue('C1', 'Наименование');
+        $sheet->setCellValue('C1', 'Наименование товара');
         $sheet->getColumnDimension('C')->setWidth(40);
-        $sheet->setCellValue('D1', 'Мин.цена');
+        $sheet->setCellValue('D1', 'Оптовая (закупочная) цена');
         $sheet->getColumnDimension('D')->setWidth(14);
-        $sheet->setCellValue('E1', 'Цена');
+        $sheet->setCellValue('E1', 'Розничная цена');
         $sheet->getColumnDimension('E')->setWidth(14);
-        $sheet->setCellValue('F1', 'Остаток');
+        $sheet->setCellValue('F1', 'Доступное количество');
         $sheet->getColumnDimension('F')->setWidth(14);
+        $sheet->setCellValue('G1', 'Наценка');
+        $sheet->getColumnDimension('G')->setWidth(14);
 
         $sheet->getStyle('1:1')->getFont()->setBold(true);
         $sheet->getStyle('1:1')->getAlignment()->setHorizontal('center');
         $sheet->getStyle('A')->getAlignment()->setHorizontal('center');
-        $sheet->getStyle('D:F')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('D:G')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('G:G')
+            ->getNumberFormat()
+            ->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_PERCENTAGE);
 
         $items = Item::withoutGlobalScope(UserScope::class)
             ->where([
@@ -75,19 +80,60 @@ class GeneratePrice implements ShouldQueue
 
             $sheet->setCellValue('C' . $row, $item->name);
 
-            $bestPriceContractorItem = $item
-                ->contractorItems()
-                ->withoutGlobalScope(UserScope::class)
-                ->orderBy('price')
-                ->first();
-            $sheet->setCellValue(
-                'D' . $row,
-                $bestPriceContractorItem ? $bestPriceContractorItem->price : ''
-            );
+            if ($item->stock === '0') {
+                $bestPriceContractorItem = $item
+                    ->contractorItems()
+                    ->withoutGlobalScope(UserScope::class)
+                    ->orderBy('price')
+                    ->first();
 
-            $sheet->setCellValue('E' . $row, $item->price);
+                $opt = $bestPriceContractorItem->price;
+            } else {
+                $opt = $item->price;
+            }
+            $sheet->setCellValue('D' . $row, $opt);
+
+            switch (true) {
+                case $opt < 51:
+                    $markup = 45;
+                    break;
+                case $opt < 101:
+                    $markup = 40;
+                    break;
+                case $opt < 151:
+                    $markup = 35;
+                    break;
+                case $opt < 201:
+                    $markup = 30;
+                    break;
+                case $opt < 251:
+                    $markup = 25;
+                    break;
+                case $opt < 301:
+                    $markup = 20;
+                    break;
+                case $opt < 401:
+                    $markup = 15;
+                    break;
+                case $opt < 600:
+                    $markup = 10;
+                    break;
+                case $opt < 700:
+                    $markup = 8;
+                    break;
+                case $opt < 1000:
+                    $markup = 7;
+                    break;
+                default:
+                    $markup = 6;
+                    break;
+            }
+
+            $sheet->setCellValue('E' . $row, "=SUM(D{$row}, D{$row} * G{$row})");
 
             $sheet->setCellValue('F' . $row, $item->stock);
+
+            $sheet->setCellValue('G' . $row, $markup / 100);
         }
 
         $writer = new Xlsx($spreadsheet);
